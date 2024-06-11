d935dc5230268cb333952b369704983066efda61
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-06-11 19:06:15 +0900

    Setting

---
+++ src/test/java/sample/talentbridge/TalentBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.talentbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
