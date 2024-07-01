ff84d91b73955cd9a01ddd028e9522a862377784
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-07-01 17:00:03 +0900

    feat: 권한필드 추가, name,email 업데이트

--- src/main/java/sample/talentbridge/domain/member/entity/Member.java
+++ src/main/java/sample/talentbridge/domain/member/entity/Member.java
@@ -6,9 +6,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import sample.talentbridge.domain.address.entity.Address;
 
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,7 +34,26 @@ public class Member {
 
     private String nickname;
 
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
     private String phoneNumber;
 
     private int age;
+
+    private String role;
+
+    public Member updateName(String newName){
+        if(newName == null && !newName.isBlank()){
+            this.name = newName;
+        }
+        return this;
+    }
+
+    public Member updateEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+
 }
