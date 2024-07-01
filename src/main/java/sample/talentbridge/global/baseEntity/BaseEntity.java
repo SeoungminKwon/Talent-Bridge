c382cc76fd8b2055b45283e3a626c6858b452765
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-07-01 17:18:43 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/sample/talentbridge/global/baseEntity/BaseEntity.java
@@ -0,0 +1,31 @@
+package sample.talentbridge.global.baseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+}
